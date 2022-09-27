0963bc51045b493e391c5f649640e201d8c7ec75
MaxwellChu34 <111820923+MaxwellChu34@users.noreply.github.com>
2022-09-26 20:45:50 -0400

    Chu_Traffic

---
+++ Traffic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    /* Class: Program
+     * Author: Maxwell Chu
+     * Purpose: Contains the Main method and AddPassenger function
+     * Restrictions: None
+     */ 
+    internal class Program
+    {
+        /* Method: Main
+         * Purpose: None
+         * Restrictions: None
+         */
+        static void Main(string[] args)
+        {
+        }
+        /* Function: AddPassenger
+         * Purpose: Accepts any object form the IPassengerCarrier interface
+         * Restrictions: None
+         */
+        public void AddPassenger(IPassengerCarrier vehicleObject)
+        {
+            vehicleObject.LoadPassenger();
+            vehicleObject.ToString();
+        }
+    }
+}
