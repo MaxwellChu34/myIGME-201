82d97e561240f8a5a81f31bb6080f7b7f5ff6e5c
MaxwellChu34 <111820923+MaxwellChu34@users.noreply.github.com>
2022-10-03 18:52:35 -0400

    Chu_DefiningClassMembers

---
+++ Chu_DefiningClassMembers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chu_DefiningClassMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass derivedClass = new MyDerivedClass();
+            derivedClass.MyString = "Never gonna give you up.";
+            Console.WriteLine(derivedClass.GetString());
+        }
+    }
+    public class MyClass
+    {
+        private string myString;
+        public virtual string GetString()
+        {
+            return myString;
+        }
+        public string MyString
+        {
+            set { myString = value; }
+        }
+    }
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            return base.GetString() + " (output from the derived class)";
+        }
+    }
+}
