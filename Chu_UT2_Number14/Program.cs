51533dc177b4559c27d796369945ac1898b2dd40
MaxwellChu34 <111820923+MaxwellChu34@users.noreply.github.com>
2022-10-23 22:04:21 -0400

    Chu_UT2_Number14

---
+++ Chu_UT2_Number14/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructToClass
+{
+    public class Friend
+    {
+        public string name;
+        public string greeting;
+        public DateTime birthdate;
+        public string address;
+    }
+    /* Class: Program
+     * Author: Maxwell Chu
+     * Purpose: Replaces struct to a public class so the original code is altered
+     * Restrictions: None
+     */
+    class Program
+    {
+        /* Method: Main
+         * Purpose: The instances are created, and the "enemy = friend;" line is commented out in order to get the same output
+         * Restrictions: None
+         */
+        static void Main(string[] args)
+        {
+            Friend friend = new Friend();
+            Friend enemy = new Friend();
+
+            // create my friend Charlie Sheen
+            friend.name = "Charlie Sheen";
+            friend.greeting = "Dear Charlie";
+            friend.birthdate = DateTime.Parse("1967-12-25");
+            friend.address = "123 Any Street, NY NY 12202";
+
+            // now he has become my enemy
+            //enemy = friend;
+
+            // set the enemy greeting and address without changing the friend variable
+            enemy.greeting = "Sorry Charlie";
+            enemy.address = "Return to sender. Address unknown.";
+
+            Console.WriteLine($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}");
+            Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
+        }
+    }
+}
