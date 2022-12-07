4e8d2d2a639b24156e25905d7081d82ca95521a6
MaxwellChu34 <111820923+MaxwellChu34@users.noreply.github.com>
2022-12-07 01:01:10 -0500

    Chu_FinalQ1

---
+++ Chu_FinalQ1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chu_FinalQ1
+{
+    internal class Program
+    {
+        public class MyStack
+        {
+            public List<int> myStack = new List<int>();
+            public void Push(int n)
+            {
+                myStack.Add(n);
+            }
+            public int? Peek()
+            {
+                if(myStack.Count > 0)
+                {
+                    return myStack[myStack.Count - 1];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            public int? Pop()
+            {
+                if(myStack.Count > 0)
+                {
+                    int? r = myStack[myStack.Count - 1];
+                    myStack.RemoveAt(myStack.Count - 1);
+                    return r;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
