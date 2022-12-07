53312617f098eac111bfa68d97c97623ccdc1a6c
MaxwellChu34 <111820923+MaxwellChu34@users.noreply.github.com>
2022-12-07 01:01:33 -0500

    Chu_FinalQ2

---
+++ Chu_FinalQ2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chu_FinalQ2
+{
+    internal class Program
+    {
+        public class MyQueue
+        {
+            public List<int> myQueue = new List<int>();
+            public void Enqueue(int n)
+            {
+                myQueue.Insert(0,n);
+            }
+            public int? Peek()
+            {
+                if (myQueue.Count > 0)
+                {
+                    return myQueue[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            public int? Dequeue()
+            {
+                if (myQueue.Count > 0)
+                {
+                    int? r = myQueue[0];
+                    myQueue.RemoveAt(0);
+                    return r;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
